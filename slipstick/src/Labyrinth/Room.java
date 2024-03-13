f719560cbdd83c157dca322149388d831a673039
Pokó Kincső Réka <pokokincso@gmail.com>
2024-03-13 12:44:23 +0100

    Room class.

---
+++ slipstick/src/Labyrinth/Room.java
@@ -0,0 +1,83 @@
+package Labyrinth;
+
+/**
+ * Labyrinth.Room class. It represents the rooms of the game.
+ * It handles the entities and items placed in the room.
+ * It handles the neighbour rooms and the accessibility of those.
+ */
+public class Room {
+    private boolean gassed;
+    private int capacity;
+
+    public Room(){
+
+    }
+
+    public int CheckCapacity(){
+
+    }
+
+    public int CheckForEntityInRoom(){
+
+    }
+
+    public void SendAllNeighbours(Room r){
+
+    }
+
+    public void SendAllItems(Room r){
+
+    }
+
+    public void SendEveryOtherItem(Room r){
+
+    }
+
+    public void SendSomeNeighbour(Room r){
+
+    }
+
+    public void AddItemToRoom(Item i){
+
+    }
+
+    public void RemoveItemFromRoom(Item i){
+
+    }
+
+    public void ReleaseToxicGas(int n){
+
+    }
+
+    public void DeactivateToxicGas(){
+
+    }
+
+    public void AddStudentToRoom(Student s){
+
+    }
+
+    public void AddProfessorToRoom(Professor p){
+
+    }
+
+    public void RemoveStudentFromRoom(Student s){
+
+    }
+
+    public void RemoveProfessorFromRoom(Professor p){
+
+    }
+
+    public boolean CanStepIn(){
+
+    }
+
+    public boolean IsGassed(){
+
+    }
+
+    void NotifyProfessors(Student s){
+
+    }
+}
