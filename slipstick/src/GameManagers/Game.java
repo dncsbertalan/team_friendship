170ba9436b2938c9a2008f196b2f8e51e64f97cc
Bene <benedekszabo12@hotmail.com>
2024-03-13 13:41:21 +0100

    init

--- slipstick/src/GameManagers/Game.java
+++ slipstick/src/GameManagers/Game.java
@@ -1,1 +1,54 @@
-#include faszom
+package GameManagers;
+import Entities.Professor;
+import Student.*;
+
+/**
+ * Initializes the game. Its main task is to handle the actions of the currently active student
+ * tracked by the RoundManager, move the professors, and serve as the core of the game, bringing
+ * everything together
+ */
+public class Game{
+    /**
+     * Indicates whether the game is in its last phase.
+     */
+    boolean lastPhase;
+    /**
+     * If lastPhase, this Student is hunted.
+     */
+    Student hunted;
+    /**
+     * List of students in the game.
+     */
+    List<Student> students;
+    /**
+     * @return List of professors in the game.
+     */
+    List<Professor> professors;
+
+    /**
+     * Returns the students list.
+     */
+    public List<Student> GetStudents();
+
+    /**
+     * @return professors
+     */
+    public List<Professor> GetProfessor();
+    /*
+
+     */
+    public void UpdateStudentMoves();
+    public void UpdateProfessorMoves();
+    public void LastPhase(Student student);
+    public void LastPhaseOff();
+    public void EndGame(boolean b);
+
+    /**
+     *
+     * @param ItemClass
+     * @param entity
+     * @return
+     */
+    public boolean CheckForItem(String ItemClass, Entity entity);
+    public void CheckForWinningCondition();
+}
---
+++ slipstick/src/GameManagers/RoundManager.java
@@ -0,0 +1,10 @@
+package GameManagers;
+
+/**
+ * Responsible for the functioning of game rounds.
+ * It determines which entity goes next and how many steps they can take.
+ * The RoundManager initiates the division/merging of rooms.
+*/
+public class RoundManager{
+ 
+}
