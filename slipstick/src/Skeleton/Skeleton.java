4713e71daab45b17fbcf01fb629f7553c87a78e0
Dancs Bertalan <dancs.bertalan44@gmail.com>
2024-03-20 17:07:51 +0100

    Created the Skeleton class.

---
+++ slipstick/src/Skeleton/Skeleton.java
@@ -0,0 +1,11 @@
+package Skeleton;
+
+/**
+ * The static Skeleton class which is used to test the use-cases defined in the week 5 documentation.
+ */
+public class Skeleton {
+
+    public static void main(String[] args) {
+        System.out.println("Pussy <33");
+    }
+}
