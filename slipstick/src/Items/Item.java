6ebe4d56ee05ac01c69ae8d87b9fbd7f1ddcc7cd
Dancs Bertalan <dancs.bertalan44@gmail.com>
2024-03-20 16:59:18 +0100

    Updated item class.

--- slipstick/src/Items/Item.java
+++ slipstick/src/Items/Item.java
@@ -2,38 +2,51 @@ package Items;
 
 import Entities.Student;
 
+/**
+ * Base class for all items in the game.
+ * Contains everything generally assosiated with items.
+ */
 public abstract class Item {
 
     public enum ProtectionType {
         none, tvsz, wetCloth, ffp2Mask
     }
 
-    private boolean activate;
+    /**
+     * Wether the item is activated or not.
+     */
+    private boolean activated;
+
+    /**
+     * The type of protection this item can provide.
+     */
     private ProtectionType protectionType = ProtectionType.none;
 
     /**
-     *
-     * @param student
+     * Called when an item is used by the student.
+     * @param student the item user
      */
-    public void UseItem(Student student) {
-
-    }
+    public abstract void UseItem(Student student);
 
     /**
-     *
+     * Activates the item.
      */
     public void ActivateItem() {
-
+        this.activated = true;
     }
 
     /**
-     *
+     * Deactivates the item.
      */
     public void DeactivateItem() {
-
+        this.activated = false;
     }
 
-    public ProtectionType getProtectionType() {
-        return protectionType;
+    /**
+     * Returns the type of protection this item can provide.
+     * @return the type of protection
+     */
+    public ProtectionType GetProtectionType() {
+        return this.protectionType;
     }
 }
