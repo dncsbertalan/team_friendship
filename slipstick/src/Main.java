67f44c655267ec74883866e7d9b8e4ad049e6295
Dancs Bertalan <dancs.bertalan44@gmail.com>
2024-03-23 16:27:25 +0100

    Removed Main, cause we don't need it.

--- slipstick/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    /**
-     * Main
-     * @param args Arguments of main
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
