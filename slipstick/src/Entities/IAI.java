b82cb06f0d8e808540029dd5a346fe18991d5272
Dancs Bertalan <dancs.bertalan44@gmail.com>
2024-03-30 23:55:13 +0100

    A base for the game loop and logic.

---
+++ slipstick/src/Entities/IAI.java
@@ -0,0 +1,9 @@
+package Entities;
+
+public interface IAI {
+
+    /**
+     * Defines the behaviour of the entity implementing the interface.
+     */
+    void AI();
+}
--- slipstick/src/GameManagers/RoundManager.java
+++ slipstick/src/GameManagers/RoundManager.java
@@ -2,88 +2,115 @@ package GameManagers;
 import Constants.GameConstants;
 import Entities.*;
 
+import java.util.ArrayList;
+
 /**
  * Responsible for the functioning of game rounds.
  * It determines which entity goes next and how many steps they can take.
  * The RoundManager initiates the division/merging of rooms.
 */
 public class RoundManager{
+
+//region Attributes
     Game game;
     int rounds = 0;
     Student activeStudent;
-    Professor activeProfessor;
+    IAI activeAIEntity;
+    ArrayList<Student> studentsLeftThisRound;
+    ArrayList<IAI> aiEntities;
+//endregion
 
-    public RoundManager(Game g){
-        game = g;
+    public RoundManager(Game game) {
+        this.game = game;
+
+        this.studentsLeftThisRound = new ArrayList<>();
+        this.studentsLeftThisRound.addAll(this.game.GetStudents());
+        this.aiEntities = new ArrayList<>();
+        this.aiEntities.addAll(this.game.GetProfessors());  // TODO: takarító
+
+        activeStudent = null;   // TODO: ennek kezdetben az első playernek kell lenni
+        activeAIEntity = null;
     }
 
-    public void Round() {
-        for (Student student : game.GetStudents()) {
-            while(!student.IsDead() && student.GetRemainingTurns() > 0) {
-                Turn(student);
-            }
-        }
-        for (Professor professor : game.GetProfessors()) {
-            while(professor.GetRemainingTurns() > 0) {
-                Turn(professor);
-            }
-        }
-        NextRound();
-    }
-    private void Turn(Entity entity) {
-        //do a barrel roll.wav
-        //controls ide
-    }
     /**
      * Sets the active student.
      * @param student the new active student
      */
-    public void SetActiveStudent(Student student){
+    public void SetActiveStudent(Student student) {
         activeStudent = student;
-    }
-
-    /**
-     * Sets the active professor.
-     * @param professor the new active professor
-     */
-    public void SetActiveProfessor(Professor professor){
-        activeProfessor = professor;
     }
 
     /**
      * @return the active student
      */
-    public Student GetActiveStudent(){
+    public Student GetActiveStudent() {
         return activeStudent;
     }
 
     /**
      * @return the active professor
      */
-    public Professor GetActiveprofessor(){
-        return activeProfessor;
+    public IAI GetActiveAIEntity() {
+        return activeAIEntity;
     }
-
-    /**
-     * Indicates that the active student finished with its turns in the current round.
-     */
-   public void StudentFinished(){
-
-   }
 
     /**
      *Start new round.
      */
     public void NextRound() {
-        rounds+=1;
-        if(rounds== GameConstants.MaxRounds){
+        rounds++;
+        if (rounds == GameConstants.MaxRounds) {
             game.EndGame(false);
         }
     }
-    /**
-     * Ends current round and notifies other classes.
-     */
-    void EndOfRound(){
 
+    /**
+     * Ends current round.
+     */
+    void EndOfRound() {
+
+        // reset the entities
+        this.studentsLeftThisRound.addAll(this.game.GetStudents());
+        this.aiEntities.addAll(this.game.GetProfessors());  // TODO: takarító
+
+        activeStudent = null;   // TODO: ennek kezdetben az első playernek kell lenni
+        activeAIEntity = null;
+
+        // TODO: kör vége logikák számlálók...
+
+        // start a new round
+        this.NextRound();
+    }
+
+    /**
+     * Ends the currently active entity's turn and activates the next.
+     */
+    void EndTurn(){
+
+        // TODO: unpickable items reset
+
+        if (activeStudent != null) {
+            studentsLeftThisRound.remove(activeStudent);
+            // Make the next student active
+            if (!studentsLeftThisRound.isEmpty()) {
+                activeStudent = studentsLeftThisRound.get(0);
+                return;
+            }
+            // if there are no students left this round
+            activeStudent = null;
+            if (!aiEntities.isEmpty()) {
+                activeAIEntity = aiEntities.get(0);
+            }
+        }
+        if (activeAIEntity != null) {
+            aiEntities.remove(activeAIEntity);
+            // Make the next entity active
+            if (!aiEntities.isEmpty()) {
+                activeAIEntity = aiEntities.get(0);
+                return;
+            }
+        }
+        // if there are no entities left this round
+        this.EndOfRound();
     }
 }
