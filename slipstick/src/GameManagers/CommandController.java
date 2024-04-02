a1dce40e307455073d6430bfa2350895fdc6a804
Dancs Bertalan <dancs.bertalan44@gmail.com>
2024-04-02 14:45:30 +0200

    COMMANDS!!!

--- slipstick/src/GameManagers/CommandController.java
+++ slipstick/src/GameManagers/CommandController.java
@@ -1,4 +1,36 @@
 package GameManagers;
 
+import GameManagers.Commands.Commands;
+import GameManagers.Commands.ICommand;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class CommandController {
+
+    private final static HashMap<String, ICommand> commands = new HashMap<>();
+    static {
+        commands.put("exit", (args) -> CommandController.getInput = false);
+        commands.put("use_item", Commands::UseItem);
+        commands.put("move", Commands::Move);
+    }
+
+    static boolean getInput = true;
+
+    public static void GetInput() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (getInput) {
+            String[] cmd = scanner.nextLine().split(" ");
+
+            ICommand command = commands.get(cmd[0]);
+            if (command == null) {
+                System.out.println("Error: Unknown command");
+            }
+            else {
+                command.execute(cmd);
+            }
+        }
+    }
 }
--- slipstick/src/Runnable/Main.java
+++ slipstick/src/Runnable/Main.java
@@ -1,12 +1,17 @@
 package Runnable;
 
+import GameManagers.CommandController;
 import GameManagers.Game;
 
 public class Main {
 
-
+    public static boolean isConsole = true;
+    public static final Game game = new Game();
 
     public static void main(String[] args) {
-        Game game = new Game();
+        //Game game = new Game();
+        //game.MainGameLoop();
+
+        CommandController.GetInput();
     }
 }
