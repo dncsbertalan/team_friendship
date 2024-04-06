1b93030245f0b3de44fcd581dff5e5f60a9f58ba
webo57 <144137468+webo57@users.noreply.github.com>
2024-04-06 14:17:33 +0200

    AirFresher item added

---
+++ slipstick/src/Items/AirFresher.java
@@ -0,0 +1,14 @@
+package Items;
+
+import Entities.Student;
+
+public class AirFresher extends Item {
+    /**
+     * Deactivates toxic gas in the room it is used in.
+     * @param student: Student using the AirFresher.
+     */
+    @Override
+    public void UseItem(Student student) {
+        student.GetCurrentRoom().DeactivateToxicGas();
+    }
+}
