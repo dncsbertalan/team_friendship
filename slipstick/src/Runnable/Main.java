a2632836d628e2666f571394a1d320d36db9fd71
Dancs Bertalan <dancs.bertalan44@gmail.com>
2024-04-24 21:06:38 +0200

    Not the utf-8 it was the fuckin ' !!!!!!

--- slipstick/src/Runnable/Main.java
+++ slipstick/src/Runnable/Main.java
@@ -12,7 +12,6 @@ public class Main {
     public static final Game game = new Game();
 
     public static void main(String[] args) {
-        System.setProperty("file.encoding", "UTF-8");
         CommandController.GetInput();
     }
 }
