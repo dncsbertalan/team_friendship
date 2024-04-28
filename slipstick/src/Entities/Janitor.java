c7399cf64a12144f1da0a558b1c6b63ac3d5e4b6
Benedek Ferenc Szabó <benedekszabo12@hotmail.com>
2024-04-28 21:30:11 +0200

    evicteveryone

--- slipstick/src/Entities/Janitor.java
+++ slipstick/src/Entities/Janitor.java
@@ -1,8 +1,10 @@
 package Entities;
 
 import Constants.GameConstants;
+import GameManagers.Commands.Commands;
 import GameManagers.Game;
 import Labyrinth.Room;
+import static Runnable.Main.os;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,46 +64,50 @@ public class Janitor extends Entity implements IAI {
         int roomID = 0;
         int nr = 0;
         do{
-            for(Student studentIter : studentsOfRoom){
-                if(studentIter.IsParalysed() == false){
+            ArrayList<Student> studentToRemove = new ArrayList<>();
+            int index = 0;
+            while (index < studentsOfRoom.size()) {
+                Student studentIter = studentsOfRoom.get(index);
+                if (!studentIter.IsParalysed()) {
                     //roomID % sizeOfList, for there might be more students than neighbouring rooms
                     Room currentNeighbour = neighboursOfRoom_.get(roomID % neighboursOfRoom_.size());
-                    if(currentNeighbour.CanStepIn()){
-                        studentIter.StepInto(currentNeighbour);
+                    if (currentNeighbour.CanStepIn()) {
+                        String asd = "move " + studentIter.GetName() + " " + currentNeighbour.GetName();
+                        Commands.Move(asd.split(" "));
+                        studentToRemove.add(studentIter);
                     }
                     roomID++;
                 }
+                index++;
             }
+            studentsOfRoom.removeAll(studentToRemove);
 
-            //if all the students couldn't fit into the neighbours of current iteration list
-            if(studentsOfRoom.isEmpty() == false){
-                //if the original still has rooms that we have not tried the neighbours of
-                if(nr < neighboursOfRoom.size()){
-                    neighboursOfRoom_ = neighboursOfRoom.get(nr).GetNeighbours();
-                    nr++;
-                }
-                //the original will change to the current iteration list and the next cycle will try and go through those room and their neighbours
-                else {
-                    nr = 0;
-                    neighboursOfRoom = neighboursOfRoom_;
-                }
-            }
-        } while(studentsOfRoom.isEmpty());
+
+        } while(!studentsOfRoom.isEmpty());
 
         do{
-            for(Professor professorIter : professorsOfRoom){
-                if(professorIter.IsParalysed() == false){
+            ArrayList<Professor> professorToRemove = new ArrayList<>();
+            int index = 0;
+            while (index < professorsOfRoom.size()) {
+                Professor professorIter = professorsOfRoom.get(index);
+                if (!professorIter.IsParalysed()) {
                     //roomID % sizeOfList, for there might be more professors than neighbouring rooms
                     Room currentNeighbour = neighboursOfRoom_.get(roomID % neighboursOfRoom_.size());
-                    if(currentNeighbour.CanStepIn()){
+                    if (currentNeighbour.CanStepIn()) {
+                        os.println("Moved " + professorIter.GetName() + " from " + professorIter.GetCurrentRoom().GetName() + " to " + currentNeighbour.GetName());
                         professorIter.StepInto(currentNeighbour);
+                        professorToRemove.add(professorIter);
                     }
                     roomID++;
                 }
+                index++;
             }
 
+            professorsOfRoom.removeAll(professorToRemove);
+
+
             //if all the professors couldn't fit into the neighbours of current iteration list
-            if(professorsOfRoom.isEmpty() == false){
+            if(!professorsOfRoom.isEmpty()){
                 //if the original still has rooms that we have not tried the neighbours of
                 if(nr < neighboursOfRoom.size()){
                     neighboursOfRoom_ = neighboursOfRoom.get(nr).GetNeighbours();
